d6e0dfd2d7bbc820a7278a9200f86033d983aa7b
Deivid21 <david.parra.ignacio@gmail.com>
2024-09-30 19:36:12 -0300

    borneo: Build MotCamera3 from prebuilt source
    Change-Id: I563e1d2d8c1c9634cc842c11c29920055b13363f
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,12 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 720
 
+# Moto Camera 3
+TARGET_MOTCAMERA3 := borneo
+TARGET_USES_MOTCAMERA3 := true
+
+$(call inherit-product, vendor/motorola/MotCamera3/motcamera3.mk)
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
